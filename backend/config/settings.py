fd16205fb65f79b1517e954473f45dd2f3ecab4d
Prateek Sharma <sharmaprateek0076@gmail.com>
2026-05-28 23:57:13 +0530

    updated production cors settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://esg-implementation.vercel.app",
+]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "uploads"
